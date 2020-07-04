70aefd253c10823de9f49084c355f12028a7b102
FesterHead <steve@kunitzer.me>
2020-07-04 12:15:19 -1000

    Updated BreedingTaskType
    Includes logging and other minor improvements.

--- src/main/java/com/leonardobishop/quests/quests/tasktypes/types/BreedingTaskType.java
+++ src/main/java/com/leonardobishop/quests/quests/tasktypes/types/BreedingTaskType.java
@@ -10,10 +10,12 @@ import com.leonardobishop.quests.quests.Task;
 import com.leonardobishop.quests.quests.tasktypes.ConfigValue;
 import com.leonardobishop.quests.quests.tasktypes.TaskType;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityBreedEvent;
 import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 
 import java.util.ArrayList;
@@ -22,10 +24,11 @@ import java.util.List;
 public final class BreedingTaskType extends TaskType {
 
     private List<ConfigValue> creatorConfigValues = new ArrayList<>();
+    private static final String AMOUNT_KEY = "amount";
 
     public BreedingTaskType() {
         super("breeding", "toasted", "Breed a set amount of animals.");
-        this.creatorConfigValues.add(new ConfigValue("amount", true, "Amount of animals to be bred"));
+        this.creatorConfigValues.add(new ConfigValue(AMOUNT_KEY, true, "Amount of animals to breed."));
     }
 
     @Override
@@ -34,52 +37,58 @@ public final class BreedingTaskType extends TaskType {
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void onBreed(CreatureSpawnEvent e) {
-        if (!e.getSpawnReason().equals(SpawnReason.BREEDING)) {
-            return;
-        }
+    public void onBreed(EntityBreedEvent event) {
+        QPlayer qPlayer = QuestsAPI.getPlayerManager().getPlayer(event.getBreeder().getUniqueId(), true);
+        QuestProgressFile questProgressFile = qPlayer.getQuestProgressFile();
 
-        Entity ent = e.getEntity();
-        List<Entity> entList = ent.getNearbyEntities(10, 10, 10);
+        for (Quest quest : super.getRegisteredQuests()) {
+            if (questProgressFile.hasStartedQuest(quest)) {
+                QuestsAPI.getQuestManager().getPlugin().getQuestsLogger().debug("§4--------------------");
+                QuestsAPI.getQuestManager().getPlugin().getQuestsLogger()
+                        .debug("              Quest: §6" + quest.getId());
+                QuestProgress questProgress = questProgressFile.getQuestProgress(quest);
 
-        if (entList.isEmpty()) {
-            return;
-        }
-        // Check if there is a player in the list, otherwise: return.
-        for (Entity current : entList) {
+                // Special code to get the incoming object for this task
+                EntityType incomingObject = event.getEntity().getType();
+                QuestsAPI.getQuestManager().getPlugin().getQuestsLogger()
+                        .debug("    Incoming object: §b" + incomingObject.toString());
 
-            if (current instanceof Player) {
-                Player player = (Player) current;
-                QPlayer qPlayer = QuestsAPI.getPlayerManager().getPlayer(player.getUniqueId(), true);
-                QuestProgressFile questProgressFile = qPlayer.getQuestProgressFile();
+                for (Task task : quest.getTasksOfType(super.getType())) {
+                    TaskProgress taskProgress = questProgress.getTaskProgress(task.getId());
+                    int taskProgressCounter = (taskProgress.getProgress() == null) ? 0
+                            : (int) taskProgress.getProgress();
 
-                for (Quest quest : super.getRegisteredQuests()) {
-                    if (questProgressFile.hasStartedQuest(quest)) {
-                        QuestProgress questProgress = questProgressFile.getQuestProgress(quest);
+                    QuestsAPI.getQuestManager().getPlugin().getQuestsLogger().debug("");
+                    QuestsAPI.getQuestManager().getPlugin().getQuestsLogger()
+                            .debug("      Checking task: §8" + task.getId());
+                    QuestsAPI.getQuestManager().getPlugin().getQuestsLogger()
+                            .debug("               Type: §8" + task.getType());
+                    QuestsAPI.getQuestManager().getPlugin().getQuestsLogger()
+                            .debug("           Progress: §d" + taskProgressCounter);
+                    QuestsAPI.getQuestManager().getPlugin().getQuestsLogger()
+                            .debug("               Need: §5" + (int) task.getConfigValue(AMOUNT_KEY));
+                    QuestsAPI.getQuestManager().getPlugin().getQuestsLogger()
+                            .debug("          Completed: §6" + taskProgress.isCompleted());
 
-                        for (Task task : quest.getTasksOfType(super.getType())) {
-                            TaskProgress taskProgress = questProgress.getTaskProgress(task.getId());
-
-                            if (taskProgress.isCompleted()) {
-                                continue;
-                            }
-
-                            int breedingNeeded = (int) task.getConfigValue("amount");
-                            int breedingProgress;
-
-                            if (taskProgress.getProgress() == null) {
-                                breedingProgress = 0;
-                            } else {
-                                breedingProgress = (int) taskProgress.getProgress();
-                            }
-
-                            taskProgress.setProgress(breedingProgress + 1);
-
-                            if (((int) taskProgress.getProgress()) >= breedingNeeded) {
-                                taskProgress.setCompleted(true);
-                            }
-                        }
+                    if (taskProgress.isCompleted()) {
+                        continue;
                     }
+
+                    int progressIncrement = 1;
+                    QuestsAPI.getQuestManager().getPlugin().getQuestsLogger()
+                            .debug("          Increment: §2" + progressIncrement);
+
+                    taskProgress.setProgress(taskProgressCounter + progressIncrement);
+                    QuestsAPI.getQuestManager().getPlugin().getQuestsLogger()
+                            .debug("       New progress: §e" + taskProgress.getProgress().toString());
+
+                    if (((int) taskProgress.getProgress()) >= (int) task.getConfigValue(AMOUNT_KEY)) {
+                        taskProgress.setCompleted(true);
+                        QuestsAPI.getQuestManager().getPlugin().getQuestsLogger().debug("           §6Completed!");
+                    }
+
+                    return;
+
                 }
             }
         }

d5e736318a874d308add39af1f515be370859920
FesterHead <steve@kunitzer.me>
2020-07-04 11:38:21 -1000

    Adjusted to Taming to be more like TamingCertain
    Includes logging and other minor improvements.

--- src/main/java/com/leonardobishop/quests/quests/tasktypes/types/TamingTaskType.java
+++ src/main/java/com/leonardobishop/quests/quests/tasktypes/types/TamingTaskType.java
@@ -9,6 +9,8 @@ import com.leonardobishop.quests.quests.Quest;
 import com.leonardobishop.quests.quests.Task;
 import com.leonardobishop.quests.quests.tasktypes.ConfigValue;
 import com.leonardobishop.quests.quests.tasktypes.TaskType;
+
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -20,10 +22,11 @@ import java.util.List;
 public final class TamingTaskType extends TaskType {
 
     private List<ConfigValue> creatorConfigValues = new ArrayList<>();
+    private static final String AMOUNT_KEY = "amount";
 
     public TamingTaskType() {
         super("taming", "LMBishop", "Tame a set amount of animals.");
-        this.creatorConfigValues.add(new ConfigValue("amount", true, "Amount of animals to be tamed."));
+        this.creatorConfigValues.add(new ConfigValue(AMOUNT_KEY, true, "Amount of animals to be tamed."));
     }
 
     @Override
@@ -37,39 +40,58 @@ public final class TamingTaskType extends TaskType {
             return;
         }
 
-        Player player = (Player) event.getOwner();
-
-        QPlayer qPlayer = QuestsAPI.getPlayerManager().getPlayer(player.getUniqueId(), true);
+        QPlayer qPlayer = QuestsAPI.getPlayerManager().getPlayer(event.getOwner().getUniqueId(), true);
         QuestProgressFile questProgressFile = qPlayer.getQuestProgressFile();
 
         for (Quest quest : super.getRegisteredQuests()) {
             if (questProgressFile.hasStartedQuest(quest)) {
+                QuestsAPI.getQuestManager().getPlugin().getQuestsLogger().debug("§4--------------------");
+                QuestsAPI.getQuestManager().getPlugin().getQuestsLogger()
+                        .debug("              Quest: §6" + quest.getId());
                 QuestProgress questProgress = questProgressFile.getQuestProgress(quest);
+
+                EntityType sourceMaterial = event.getEntity().getType();
+                QuestsAPI.getQuestManager().getPlugin().getQuestsLogger()
+                        .debug("    Source material: §b" + sourceMaterial.toString());
 
                 for (Task task : quest.getTasksOfType(super.getType())) {
                     TaskProgress taskProgress = questProgress.getTaskProgress(task.getId());
+                    int taskProgressCounter = (taskProgress.getProgress() == null) ? 0
+                            : (int) taskProgress.getProgress();
+
+                    QuestsAPI.getQuestManager().getPlugin().getQuestsLogger().debug("");
+                    QuestsAPI.getQuestManager().getPlugin().getQuestsLogger()
+                            .debug("      Checking task: §8" + task.getId());
+                    QuestsAPI.getQuestManager().getPlugin().getQuestsLogger()
+                            .debug("               Type: §8" + task.getType());
+                    QuestsAPI.getQuestManager().getPlugin().getQuestsLogger()
+                            .debug("           Progress: §d" + taskProgressCounter);
+                    QuestsAPI.getQuestManager().getPlugin().getQuestsLogger()
+                            .debug("               Need: §5" + (int) task.getConfigValue(AMOUNT_KEY));
+                    QuestsAPI.getQuestManager().getPlugin().getQuestsLogger()
+                            .debug("          Completed: §6" + taskProgress.isCompleted());
 
                     if (taskProgress.isCompleted()) {
                         continue;
                     }
 
-                    int tamesNeeded = (int) task.getConfigValue("amount");
+                    int progressIncrement = 1;
+                    QuestsAPI.getQuestManager().getPlugin().getQuestsLogger()
+                            .debug("          Increment: §2" + progressIncrement);
 
-                    int progressTamed;
-                    if (taskProgress.getProgress() == null) {
-                        progressTamed = 0;
-                    } else {
-                        progressTamed = (int) taskProgress.getProgress();
-                    }
+                    taskProgress.setProgress(taskProgressCounter + progressIncrement);
+                    QuestsAPI.getQuestManager().getPlugin().getQuestsLogger()
+                            .debug("       New progress: §e" + taskProgress.getProgress().toString());
 
-                    taskProgress.setProgress(progressTamed + 1);
-
-                    if (((int) taskProgress.getProgress()) >= tamesNeeded) {
+                    if (((int) taskProgress.getProgress()) >= (int) task.getConfigValue(AMOUNT_KEY)) {
                         taskProgress.setCompleted(true);
+                        QuestsAPI.getQuestManager().getPlugin().getQuestsLogger().debug("           §6Completed!");
                     }
+
+                    return;
+
                 }
             }
         }
     }
-
 }

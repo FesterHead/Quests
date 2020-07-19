ff36ad0332c14f5e63b2afc079f5490a36bdf7fe
FesterHead <steve@kunitzer.me>
2020-07-19 10:00:13 -1000

    Added enchant task type back.

---
+++ src/main/java/com/leonardobishop/quests/quests/tasktypes/types/EnchantItemTaskType.java
@@ -0,0 +1,36 @@
+package com.leonardobishop.quests.quests.tasktypes.types;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.leonardobishop.quests.quests.tasktypes.ConfigValue;
+import com.leonardobishop.quests.quests.tasktypes.TaskType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.enchantment.EnchantItemEvent;
+
+public final class EnchantItemTaskType extends TaskType {
+
+  private List<ConfigValue> creatorConfigValues = new ArrayList<>();
+
+  public EnchantItemTaskType() {
+    super("enchant", "toasted, FesterHead", "Enchant items.");
+    this.creatorConfigValues
+        .add(new ConfigValue(AMOUNT_KEY, true, "The number of items to enchant."));
+    this.creatorConfigValues
+        .add(new ConfigValue(ITEM_KEY, false, "If supplied, the specific item to enchant."));
+    this.creatorConfigValues
+        .add(new ConfigValue(PRESENT_KEY, false, "Optional present-tense action verb."));
+    this.creatorConfigValues
+        .add(new ConfigValue(PAST_KEY, false, "Optional past-tense action verb."));
+  }
+
+  @Override
+  public List<ConfigValue> getCreatorConfigValues() {
+    return creatorConfigValues;
+  }
+
+  @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+  public void onEnchant(EnchantItemEvent event) {
+    processMaterial(event.getItem().getType(), event.getEnchanter().getUniqueId(), 1);
+  }
+}

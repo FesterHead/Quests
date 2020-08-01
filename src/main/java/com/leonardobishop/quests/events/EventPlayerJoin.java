9cb1d22337091cfcfa89bf1fd66da0aa26667a17
FesterHead <steve@kunitzer.me>
2020-08-01 08:33:47 -1000

    Feature cleanup

--- src/main/java/com/leonardobishop/quests/events/EventPlayerJoin.java
+++ src/main/java/com/leonardobishop/quests/events/EventPlayerJoin.java
@@ -3,7 +3,6 @@ package com.leonardobishop.quests.events;
 import java.util.UUID;
 
 import com.leonardobishop.quests.Quests;
-import com.leonardobishop.quests.obj.Messages;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -21,10 +20,6 @@ public class EventPlayerJoin implements Listener {
   public void onEvent(PlayerJoinEvent event) {
     UUID playerUuid = event.getPlayer().getUniqueId();
     plugin.getPlayerManager().loadPlayer(playerUuid, false);
-    if (plugin.getDescription().getVersion().contains("beta")
-        && event.getPlayer().hasPermission("quests.admin")) {
-      event.getPlayer().sendMessage(Messages.BETA_REMINDER.getMessage());
-    }
   }
 
 }
--- src/main/java/com/leonardobishop/quests/obj/Options.java
+++ src/main/java/com/leonardobishop/quests/obj/Options.java
@@ -9,17 +9,16 @@ import java.util.List;
 public enum Options {
 
   CATEGORIES_ENABLED("options.categories-enabled"), TRIM_GUI_SIZE(
-      "options.trim-gui-size"), QUESTS_START_LIMIT("options.quest-started-limit"), TITLES_ENABLED(
-          "options.titles-enabled"), GUI_HIDE_LOCKED(
+      "options.trim-gui-size"), QUESTS_START_LIMIT(
+          "options.quest-started-limit"), TITLES_ENABLED("options.titles-enabled"), GUI_HIDE_LOCKED(
               "options.gui-hide-locked"), GUI_HIDE_QUESTS_NOPERMISSION(
                   "options.gui-hide-quests-nopermission"), GUI_HIDE_CATEGORIES_NOPERMISSION(
                       "options.gui-hide-categories-nopermission"), GUITITLE_QUESTS_CATEGORY(
                           "options.guinames.quests-category"), GUITITLE_QUESTS(
-                              "options.guinames.quests-menu"), GUITITLE_DAILY_QUESTS(
-                                  "options.guinames.daily-quests"), GUITITLE_QUEST_CANCEL(
-                                      "options.guinames.quest-cancel"), ALLOW_QUEST_CANCEL(
-                                          "options.allow-quest-cancel"), QUEST_AUTOSTART(
-                                              "options.quest-autostart");
+                              "options.guinames.quests-menu"), GUITITLE_QUEST_CANCEL(
+                                  "options.guinames.quest-cancel"), ALLOW_QUEST_CANCEL(
+                                      "options.allow-quest-cancel"), QUEST_AUTOSTART(
+                                          "options.quest-autostart");
 
   private final String path;
 

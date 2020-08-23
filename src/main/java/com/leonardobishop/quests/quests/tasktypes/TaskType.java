7292d78a9fb61e21da59484b6fbdbf4eeb9de8b4
FesterHead <steve@kunitzer.me>
2020-08-23 09:48:09 -1000

    Fixed logic screw up.
    if (no reverse configured or reverse is true) and increment is less than 0

--- src/main/java/com/leonardobishop/quests/quests/tasktypes/TaskType.java
+++ src/main/java/com/leonardobishop/quests/quests/tasktypes/TaskType.java
@@ -167,9 +167,10 @@ public abstract class TaskType implements Listener {
             questLogger.debug("                     §aMatch!");
             questLogger.debug("          Increment: §2" + increment);
 
-            // if reverse key is not present or false, and increment is < 0, then skip task progress
-            if (Objects.isNull(task.getConfigValue(REVERSE_KEY))
-                || ((boolean) (task.getConfigValue(REVERSE_KEY)) && increment < 0)) {
+            // if reverse key is not present or both true and increment is < 0, then skip task
+            // progress
+            if ((Objects.isNull(task.getConfigValue(REVERSE_KEY))
+                || (boolean) (task.getConfigValue(REVERSE_KEY))) && increment < 0) {
               questLogger.debug("                     §aReverse progression skipped!");
             } else {
               taskProgress.setProgress(taskProgressCounter + increment);
